4a0cea909a584a1c230fd98d7a3c306db4de6697
wakaru <wakaru44@gmail.com>
2016-05-10 00:55:18 +0200

    me no likey tmp dirs

--- tool_create_inventory.sh
+++ tool_create_inventory.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create a simple inventory holding all the machines found in a cloud formation stack called personal.
-mkdir -p tmp
+mkdir -p .tmp
 # The destination inventory file
-INVFILE=tmp/hosts.tmp 
+INVFILE=.tmp/hosts.tmp 
 IDNS=$( aws ec2 describe-instances  | grep "ublicDns\|stack-name" | grep ec2 | perl -pe 's|.*Dns.*"(ec2-.*)".*|\1|')
 
 #reset the file
